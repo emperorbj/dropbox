14d5d04f81b9d6161dd92aac380d544e7b7f0e84
emperorbj <cwaku96@gmail.com>
2024-05-04 23:43:49 -0700

    changed width of home page container

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <main className=" flex justify-center items-center w-full h-screen">
-      <h1 className="bg-purple-500 p-[200px] text-center w-[100px] h-[80px]">Drop box</h1>
+      <h1 className="bg-purple-500 p-[200px] text-center w-[90px] h-[80px]">Drop box</h1>
     </main>
   );
 }
