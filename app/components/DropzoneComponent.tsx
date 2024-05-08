2b62d9d5a45a8cb8db413547069067aaefd0eb51
emperorbj <cwaku96@gmail.com>
2024-05-07 17:41:55 -0700

    added drag and drop functionality

--- app/components/DropzoneComponent.tsx
+++ app/components/DropzoneComponent.tsx
@@ -1,10 +1,18 @@
 "use client"
+import { cn } from '@/lib/utils';
 import React from 'react';
 import Dropzone from 'react-dropzone'
 
 const DropzoneComponent = () => {
+    const onDrop = (acceptedFiles: File[]) =>{
+        acceptedFiles.forEach((file) =>{
+            const reader = new FileReader();
 
-    // max file size 20mb
+            
+        });
+    }
+
+    // max file size 20mb 
     const maxSize = 20971520;
 
     return (
@@ -20,8 +28,12 @@ const DropzoneComponent = () => {
         fileRejections.length > 0 && fileRejections[0].file.size > maxSize;
 
         return (
-        <section>
-        <div {...getRootProps()}>
+        <section className='m-4'>
+        <div {...getRootProps()}
+        className={cn(
+            "w-full h-52 flex justify-center items-center p-5 border border-dashed rounded-lg text-center",
+            isDragActive ? "bg-[#035FFE] text-white animation-pulse" : "bg-slate-100/50 dark:bg-slate-800/80 text-slate-400"
+        )}>
             <input {...getInputProps()} />
             {!isDragActive && "click here or drop a file to upload"}
             {isDragActive && !isDragReject && "Drop to upload this file"}
