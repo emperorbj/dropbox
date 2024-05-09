fc54ce010e8088ee2e43c0629d3dc0320f62f280
emperorbj <cwaku96@gmail.com>
2024-05-09 00:08:40 -0700

    creating skeleton files load

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,10 +2,19 @@ import React from 'react'
 import DropzoneComponent from '../components/DropzoneComponent'
 import { collection, getDocs } from 'firebase/firestore'
 import { db } from '@/firebase'
+import { FileType } from '@/typings'
 
 const Dashboard = async () => {
 
     const docsResults = await getDocs(collection(db, "files"))
+    const skeletonFiles: FileType[] = docsResults.docs.map(doc =>({
+        id:doc.id,
+        filename: doc.data().filename || doc.id,
+        timestamp: new Date(doc.data().timestamp?.seconds * 1000) || undefined,
+        downloadURL:doc.data().downLoadURL,
+        type: doc.data().type,
+        size: doc.data().size,
+    }))
     return (
         <div>
         <DropzoneComponent/>
