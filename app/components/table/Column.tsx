8b08722232f8d079037e3116c61366d2263e1da3
emperorbj <cwaku96@gmail.com>
2024-05-10 05:37:14 -0700

    functionality for files

--- app/components/table/Column.tsx
+++ app/components/table/Column.tsx
@@ -2,11 +2,30 @@
 import prettyBytes from 'pretty-bytes';
 import { FileType } from "@/typings"
 import { ColumnDef } from "@tanstack/react-table"
+import { FileIcon, defaultStyles } from 'react-file-icon';
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 
 export const columns: ColumnDef<FileType>[] = [
+
+    {
+        accessorKey: "type",
+        header: "type",
+        cell:({ renderValue,...props}) =>{
+            const type = renderValue() as string;
+            const extension: string = type.split("/")[1];
+        return (
+            <div className='w-10'>
+                <FileIcon 
+                extension={extension}
+                labelColor={COLOR_EXTENSION_MAP[extension]}
+                {...defaultStyles[extension]}
+                />
+            </div>
+        )
+        }
+    }
     {
         accessorKey: "filename",
         header: "filename",
